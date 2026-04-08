f8b69819f792e16c29cc67f94d6bda27c4389727
Yann <yann@gmail.com>
2026-04-08 09:10:54 +0700

    Praktikum Jobsheet 6 Searching MB

---
+++ MahasiswaBerprestasi22.java
@@ -0,0 +1,88 @@
+public class MahasiswaBerprestasi22 {
+    Mahasiswa22[] listMhs = new Mahasiswa22[10]; // diperbesar
+    int idx;
+
+    int sequentialSearching(double cari) {
+        int posisi = -1;
+        for (int j = 0; j < idx; j++) { // pakai idx
+            if (listMhs[j].ipk == cari) {
+                posisi = j;
+                break;
+            }
+        }
+        return posisi;
+    }
+
+    void tampilPosisi(Double x, int pos) {
+        if (pos != -1) {
+            System.out.println("Data mahasiswa dengan IPK :" + x + " ditemukan pada indeks " + pos);
+        } else {
+            System.out.println("Data " + x + " tidak ditemukan");
+        }
+    }
+
+    void tampilDataSearch(Double x, int pos) {
+        if (pos != -1) {
+            System.out.println("nim\t : " + listMhs[pos].nim);
+            System.out.println("nama\t : " + listMhs[pos].nama);
+            System.out.println("kelas\t : " + listMhs[pos].kelas);
+            System.out.println("ipk\t : " + x);
+        } else {
+            System.out.println("Data mahasiswa dengan IPK " + x + " tidak ditemukan");
+        }
+    }
+
+    void tambah(Mahasiswa22 m) {
+        if (idx < listMhs.length) {
+            listMhs[idx] = m;
+            idx++;
+        } else {
+            System.out.println("Data sudah penuh");
+        }
+    }
+
+    void tampil() {
+        for (int i = 0; i < idx; i++) {
+            listMhs[i].tampilInformasi();
+            System.out.println("-------------------");
+        }
+    }
+
+    void bubbleSort() {
+        for (int i = 0; i < idx - 1; i++) {
+            for (int j = 1; j < idx - i; j++) {
+                if (listMhs[j].ipk > listMhs[j - 1].ipk) {
+                    Mahasiswa22 tmp = listMhs[j];
+                    listMhs[j] = listMhs[j - 1];
+                    listMhs[j - 1] = tmp;
+                }
+            }
+        }
+    }
+
+    void selectionSort() {
+        for (int i = 0; i < idx - 1; i++) {
+            int idxMin = i;
+            for (int j = i + 1; j < idx; j++) {
+                if (listMhs[j].ipk < listMhs[idxMin].ipk) {
+                    idxMin = j;
+                }
+            }
+            Mahasiswa22 tmp = listMhs[idxMin];
+            listMhs[idxMin] = listMhs[i];
+            listMhs[i] = tmp;
+        }
+    }
+
+    void insertionSort() {
+        for (int i = 1; i < idx; i++) {
+            Mahasiswa22 tmp = listMhs[i];
+            int j = i;
+            while (j > 0 && listMhs[j - 1].ipk > tmp.ipk) {
+                listMhs[j] = listMhs[j - 1];
+                j--;
+            }
+            listMhs[j] = tmp;
+        }
+    }
+}
